714560067736d4bd202f4a18606acc1ff642c210
Ali Reza <amosavi@ualberta.ca>
2019-07-17 15:57:53 -0600

    cleaned up files

---
+++ Apollo-Studio/Assets/Scripts/Node/SceneOneScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: where to store this file?? Ask Brandon
+public class Sphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        return 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // driver function to get input
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Select Stage (node GameObject)
+                if (hit.transform.name == "Sphere")
+                {
+                    // play sound 1 
+                    //SceneManager.LoadScene("SceneTwo");
+                    Debug.Log("click 1");
+                }
+            }
+        }
+        // a different input 
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Select Stage
+                if (hit.transform.name == "Sphere")
+                {
+                    // play sound 2 
+                    //SceneManager.LoadScene("SceneTwo");
+                    Debug.Log("click 2");
+                }
+            }
+        }
+        // TODO: handle more cases here
+        // ...
+    }
+
+}
