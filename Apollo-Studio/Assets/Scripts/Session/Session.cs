f26ddc34ca3067447ba3039e540cc74cb463a9f3
bjzuk <bjzukows@ualberta.ca>
2019-07-25 16:11:45 -0400

    fxing dependency errors

--- Apollo-Studio/Assets/Scripts/Session/Session.cs
+++ Apollo-Studio/Assets/Scripts/Session/Session.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Session : MonoBehaviour {
     private List<User> users = new List<User>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        // static SceneManager Class from Unity 
+        SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
